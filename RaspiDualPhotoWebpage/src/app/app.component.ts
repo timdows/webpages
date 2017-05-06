fef0c4afc1d4599c1e3869928f38fd0342c5793c
timdows <tim@inexpro.nl>
2017-05-06 17:17:04 +0200

    Change to pug and scss

--- RaspiDualPhotoWebpage/src/app/app.component.ts
+++ RaspiDualPhotoWebpage/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.pug',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
 }
