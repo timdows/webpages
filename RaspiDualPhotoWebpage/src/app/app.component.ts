87fb702a978949d19c10ce7a34728331764825b8
timdows <tim@inexpro.nl>
2017-05-06 17:53:12 +0200

    Display two pictures on page

--- RaspiDualPhotoWebpage/src/app/app.component.ts
+++ RaspiDualPhotoWebpage/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Configuration } from "app/app.configuration";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.pug',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+	private topImages = ["1.jpg", "2.jpg"];
+	private bottomImages = ["2.jpg", "1.jpg"];
+
+	topImage: string;
+	bottomImage: string;
+
+	constructor(private configuration: Configuration){}
+
+	ngOnInit(): void {
+		this.topImage = `url(${this.configuration.ImageLocation}/1.jpg)`;
+		this.bottomImage = `url(${this.configuration.ImageLocation}/2.jpg)`;
+	}
+
+
 }
