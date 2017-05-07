c2551547371703b1ec85d89136bf30e74a77efe3
timdows <tim@inexpro.nl>
2017-05-07 12:11:37 +0200

    Move images and make list method

--- RaspiDualPhotoWebpage/server.js
+++ RaspiDualPhotoWebpage/server.js
@@ -1,12 +1,20 @@
 var express = require('express')
 var app = express()
+var dir = require('node-dir');
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
+app.use(express.static('images'));
+
+app.get('/images.json', function (req, res) {
+	dir.files('images', function(err, files) { 
+		res.send(files);
+	});
+});
 
 app.get('/', function (req, res) {
-  res.render("index.html");
-})
+	res.render("index.html");
+});
 
 app.listen(3000, function () {
-  console.log('Node js server running on port 3000!')
-})
+	console.log('Node js server running on port 3000!')
+});
