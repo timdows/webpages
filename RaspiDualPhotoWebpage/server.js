dc1ac0c46ed8e269b3860ad89df2189e4070cea0
timdows <tim@inexpro.nl>
2017-05-12 13:51:16 +0200

    Enable set display on off via http get

--- RaspiDualPhotoWebpage/server.js
+++ RaspiDualPhotoWebpage/server.js
@@ -1,17 +1,30 @@
 var express = require('express')
 var app = express()
 var dir = require('node-dir');
+var exec = require('child_process').exec;
 
 app.use(express.static('dist'));
 app.use('/images', express.static(__dirname + '/images'));
 
 app.get('/images.json', function (req, res) {
-	dir.files('images', function(err, files) {
+	dir.files('images', function (err, files) {
 		result = [];
 		files.forEach(file => {
 			result.push(file.replace(/\\/g, "/"));
 		});
 		res.send(result);
+	});
+});
+
+app.get('/display-off', function (req, res) {
+	exec("sudo tvservice -o", function (error, stdout, stderr) {
+		res.send("Display off");
+	});
+	
+});
+app.get('/display-on', function (req, res) {
+	exec("sudo tvservice -p", function (error, stdout, stderr) {
+		res.send("Display on");
 	});
 });
 
